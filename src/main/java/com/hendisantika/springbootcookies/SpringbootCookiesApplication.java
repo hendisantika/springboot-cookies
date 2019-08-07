e69e67bb8972b65d554f067ef301bab4f9aeecc8
Hendi Santika <hendisantika@gmail.com>
2019-08-08 06:43:56 +0700

    #1 - Initial Project setup

---
+++ src/main/java/com/hendisantika/springbootcookies/SpringbootCookiesApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootcookies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCookiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCookiesApplication.class, args);
+    }
+
+}
